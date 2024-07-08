f3aa86811e3ea35f1160b721e12951b454476592
Prytula Dmytro Vitaliyovych <dimamon02@gmail.com>
2024-07-08 14:49:29 +0300

    done

---
+++ DataAccess/DataAccess.cs
@@ -0,0 +1,16 @@
+﻿using Football.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Football.DataAccess
+{
+    public class FootballContext : DbContext
+    {
+        public DbSet<Team> Teams { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DIMAMONDESKTOP\SQLEXPRESS;Database=Football;Trusted_Connection=True;");
+        }
+    }
+}
--- dz13.06/Program.cs
+++ dz13.06/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Football.Models;
+using Football.DataAccess;
+
+namespace dz13._06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new FootballContext())
+            {
+
+                context.Database.EnsureCreated();
+
+
+                var teams = new List<Team>
+                {
+                    new Team { TeamName = "Dynamo", City = "Kyiv", Wins = 10, Losses = 2, Draws = 3 },
+                    new Team { TeamName = "Shakhtar", City = "Donetsk", Wins = 9, Losses = 3, Draws = 3 },
+                    new Team { TeamName = "Karpaty", City = "Lviv", Wins = 5, Losses = 7, Draws = 3 },
+                    new Team { TeamName = "Chornomorets", City = "Odesa", Wins = 4, Losses = 8, Draws = 3 },
+                    new Team { TeamName = "Metalist", City = "Kharkiv", Wins = 3, Losses = 9, Draws = 3 }
+                };
+
+                context.Teams.AddRange(teams);
+                context.SaveChanges();
+
+
+                var allTeams = context.Teams.ToList();
+                foreach (var team in allTeams)
+                {
+                    Console.WriteLine($"Name: {team.TeamName}, City: {team.City}, Wins: {team.Wins}, Losses: {team.Losses}, Draws: {team.Draws}");
+                }
+            }
+        }
+    }
+}
